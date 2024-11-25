9e81fbbbe98a6f89dffac411061daac45be48cc4
shreya251-oss <Shreyasingh102003000@gmail.com>
2024-11-25 14:30:27 +0530

    Create visualizer components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { sortingAlgorithms } from "../common/config";
+import { useData } from "../common/store";
+import shallow from "zustand/shallow";
+import './button.css';
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    "aria-controls": `scrollable-auto-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export function NavBar() {
+  const classes = useStyles();
+
+  const [algorithm, setAlgorithm] = useData(
+    (state) => [state.algorithm, state.setAlgorithm],
+    shallow
+  );
+
+  return (
+    <div className={classes.root}>
+      <div id="d1"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+       <a href="https://thegr8binil.github.io/Algo-Visualizer/">
+        <h2 id="h1">Algo - Visualizer (Sorting)</h2></a>
+        <button id="b1">
+        <a href="https://github.com/shreya251-oss/sorting-visualizer.git" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </button>
+      </div>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={algorithm}
+          onChange={(event, id) => setAlgorithm(id)}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          {sortingAlgorithms.map((algorithm) => (
+            <Tab
+              label={algorithm.title}
+              {...a11yProps(0)}
+              key={algorithm.title}
+            />
+          ))}
+          <Tab label="All" {...a11yProps(6)} />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
+}
